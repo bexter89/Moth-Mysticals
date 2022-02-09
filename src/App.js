2c6b59b4b75eb664509e3d7486e31565fa60e700
Rebekah Cruz <rebekah.cruz702@gmail.com>
2022-02-09 17:31:26 -0600

    add favicons

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ const App = () => {
     <div className="App">
     <Helmet>
         <title>Moth Mysticals - Home</title>
+        <link rel='canonical' href='https://moth-mysticals-v2.netlify.app/' />
         <meta
           name="description"
           content="get information about Moth Mysticals"
