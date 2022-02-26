9c238610486b271511d9f4c24f808a76faf8c588
Rebekah Cruz <rebekah.cruz702@gmail.com>
2022-02-25 20:50:26 -0600

    make urls strings

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Popper from '@popperjs/core';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from "react-router-dom";
 import { DisplayProvider } from './components/DisplayContext';
 import App from './App';
