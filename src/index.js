8133668f275e5061539380f10871821fc6b876fa
Rebekah Cruz <rebekah.cruz702@gmail.com>
2022-02-25 20:48:01 -0600

    try replacing fonts

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Popper from '@popperjs/core';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { BrowserRouter } from "react-router-dom";
+import { DisplayProvider } from './components/DisplayContext';
 import App from './App';
 import Footer from './components/Footer'
-import Popper from '@popperjs/core';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import { DisplayProvider } from './components/DisplayContext';
-import PaloSanto from './fonts/palosanto-webfont.woff'
-import './fonts/palosanto-webfont.woff2'
-import './fonts/PaloSanto.ttf'
-import './fonts/PaloSanto.eot'
-import './fonts/PaloSanto.otf'
+import PaloSanto from './fonts/PaloSanto.eot'
 import Displace from './fonts/Serebryakov-Displace.eot'
-import './fonts/Serebryakov-Displace.otf'
-import './fonts/Serebryakov_-_Displace.ttf'
-import './fonts/serebryakov_-_displace-webfont.woff'
-import './fonts/serebryakov_-_displace-webfont.woff2'
 import './styles.css'
 import './fonts.css'
 
