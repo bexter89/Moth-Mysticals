7eaa46b0ef6ce62ba1a13627c9016cb465ef3360
Rebekah Cruz <rebekah.cruz702@gmail.com>
2022-02-26 18:01:05 -0600

    adjust stylesheets on index.html

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Popper from "@popperjs/core";
-import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import NavBar from './components/NavBar'
