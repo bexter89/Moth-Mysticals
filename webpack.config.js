fd05e47a52bbac1a3ee16d03573ee1807ddc1310
Rebekah Cruz <rebekah.cruz702@gmail.com>
2022-02-27 14:44:09 -0600

    remove firebase

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   mode: 'development',
   entry: "./public/index.js",
   output: {
-    path:path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
