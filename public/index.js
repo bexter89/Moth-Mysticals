669d5519a075c7b00d62c75e18c9b0328bee8507
Rebekah Cruz <rebekah.cruz702@gmail.com>
2022-02-27 14:04:18 -0600

    update firebase

--- public/index.js
+++ public/index.js
@@ -4,7 +4,7 @@ import Popper from "@popperjs/core";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import App from "./App";
+import App from "../src/App";
 import PaloSanto from "../public/fonts/PaloSanto.ttf";
 import Displace from "../public/fonts/Displace.ttf";
 import revicons from '../public/fonts/revicons.ttf'
